b37f3846b61f931bb3d38bfb07df77429c120c17
Dilippurohit47 <dilippurohit204@gmail.com>
2024-12-14 11:07:14 +0530

    added delete information controller function and route

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -22,7 +22,6 @@ export const createProfile = async (req: Request, res: Response) => {
     const mergedObject = updatedInformation.reduce((acc, curr) => {
       return { ...acc, ...curr };
     }, {});
-    console.log(mergedObject)
     const confirm = await prisma.profile.update({
       where: {
         userId: userId,
@@ -54,12 +53,55 @@ export const getInformation = async (req: Request, res: Response) => {
         userId,
       },
     });
-
     return res.status(200).json({
       message: "Information fetched successfully",
       data: profile,
     });
   } catch (error) {
     errorResponse(res, 500, "internal server error");
+  }
+};
+
+export const deleteData = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const { keyToDelete, valueToDelete } = req.body;
+
+    if (!keyToDelete || !valueToDelete) {
+      return errorResponse(res, 400, "Data is already deleted or try again");
+    }
+
+    const data = await prisma.profile.findUnique({
+      where: {
+        userId: userId,
+      },
+      select: {
+        information: true,
+      },
+    });
+
+    const information = data.information;
+
+    const updateData = Object.keys(information[0]).reduce((result, key) => {
+      if (key != keyToDelete || information[0][key] != valueToDelete) {
+        result[key] = information[0][key];
+      }
+      return result;
+    }, {});
+
+    await prisma.profile.update({
+      where: {
+        userId: userId,
+      },
+      data: {
+        information: [updateData],
+      },
+    });
+
+    return res.status(200).json({
+      message: "Information deleted successfully",
+    });
+  } catch (error) {
+    errorResponse(res, 500, "Internals server error");
   }
 };
--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express"
-import { createProfile, getInformation } from "../controllers/profileController.js"
+import { createProfile, deleteData, getInformation } from "../controllers/profileController.js"
 
 const app = express.Router()
 app.post("/save-information/:userId",createProfile)
 app.get("/get-information/:userId",getInformation)
+app.post("/delete-information/:userId",deleteData)
 export default app
